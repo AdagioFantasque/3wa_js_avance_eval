f5f4cab199aa2a570984ba82d185d30fe8775223
AdagioFantasque <adagiofantasque@gmail.com>
2023-10-06 11:38:53 +0200

    class apiCinemas

---
+++ js/modules/Api.js
@@ -0,0 +1,33 @@
+class ApiCinemas {
+    #apiUrl = 'https://data.culture.gouv.fr/api/records/1.0/search/?dataset=etablissements-cinematographiques';
+
+    constructor() {
+      this.#apiUrl = apiUrl;
+    }
+  
+    async fetchCinemas() {
+      try {
+        const response = await fetch(this.apiUrl);
+        if (!response.ok) {
+          throw new Error(`Erreur lors de la récupération des cinémas. Code : ${response.status}`);
+        }
+  
+        const data = await response.json();
+        return data.records.map(record => {
+          const cinema = record.fields;
+          return {
+            name: cinema.nom,
+            address: cinema.adresse,
+            city: cinema.commune,
+            seats: cinema.fauteuils
+          };
+        });
+      } catch (error) {
+        console.error('Erreur lors de la récupération des cinémas :', error);
+        throw error;
+      }
+    }
+  }
+  
+  export default ApiCinemas;
+  
